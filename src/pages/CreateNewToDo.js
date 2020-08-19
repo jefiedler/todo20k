14287745aa71bd94067ae3bb25ce37c11d16ea4e
jefiedler <jonas.fiedler@magenta.de>
2020-08-19 11:38:16 +0200

    Add first formular input

--- src/pages/CreateNewToDo.js
+++ src/pages/CreateNewToDo.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function CreateNewToDo(props) {
+function CreateNewToDo() {
+    const [value, setValue] = React.useState("");
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        console.log(value);
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            Test
-        </div>
+    <>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Task:
+            <input type="text" value={value} onChange={handleChange} />
+            </label>
+            <input type="submit" value="Send" />
+        </form>
+        <footer>
+            <Link to="/todo">ToDo-Liste</Link>
+        </footer>
+    </>
     );
 }
 
 export default CreateNewToDo;
+
--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -3,6 +3,7 @@ import Title from '../components/Title';
 import List from '../components/List/List';
 import ListItem from '../components/List/ListItem';
 import {fetchToDos} from '../api/fetchToDos';
+import { Link } from 'react-router-dom';
 
 function ToDoList() {
     const [toDos, setToDos] = React.useState(null);
@@ -27,6 +28,9 @@ function ToDoList() {
             ))}
           </List>
         </main>
+        <footer>
+          <Link to="/createNewTask">Create new Task</Link>
+        </footer>
       </>
     );
   }
