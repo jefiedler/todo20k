4cbee557e8f20e09ef0607f3050c82f2620af4e5
jefiedler <jonas.fiedler@magenta.de>
2020-08-20 10:39:56 +0200

    Finish error handling

--- src/pages/CreateNewToDo.js
+++ src/pages/CreateNewToDo.js
@@ -5,6 +5,8 @@ import { postToDos } from '../api/fetchToDos';
 function CreateNewToDo() {
     const [task, setTask] = React.useState("");
     const [author, setAuthor] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(false);
 
     function handleTaskChange(event) {
         setTask(event.target.value);
@@ -16,15 +18,23 @@ function CreateNewToDo() {
 
     async function handleSubmit(event) {
         event.preventDefault();
-        await postToDos({
-            task,
-            author,
-            createdAt: Date.now(),
-        });
-        setTask("");
-        setAuthor("");
+        setLoading(true);
+        setError(false);
+        try {
+            await postToDos({
+                task,
+                author,
+                createdAt: Date.now(),
+            });
+            setTask("");
+            setAuthor("");
+        } catch (error){
+            console.error(error);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
     }
-
 
     return (
     <>
@@ -37,7 +47,8 @@ function CreateNewToDo() {
                 Author:
             <input type="text" value={author} onChange={handleAuthorChange} />
             </label>
-            <input type="submit" value="Send" disabled={!task || !author}/>
+            <input type="submit" value="Send" disabled={!task || !author || loading}/>
+            {error && <p>Something bad happened. Please try again.</p>}
         </form>
         <footer>
             <Link to="/todo">ToDo-Liste</Link>
